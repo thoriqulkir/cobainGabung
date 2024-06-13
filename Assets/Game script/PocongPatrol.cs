132b97e5ccfd43705a6a79fa366dd362ca9cf502
Muhammad Aldin <aldinm195@gmail.com>
2024-06-13 17:02:41 +0700

    Cutscene Perumahan

---
+++ Assets/Game script/PocongPatrol.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PocongPatrol : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Transform currentpoint;
+    public float speed;
+
+    private void Start() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentpoint = pointB.transform;
+    }
+
+    private void Update() 
+    {
+        Vector2 point = currentpoint.position - transform.position;
+        if (currentpoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentpoint.position) < 5 && currentpoint == pointB.transform)
+        {
+            flip();
+            currentpoint = pointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentpoint.position) < 5 && currentpoint == pointA.transform)
+        {
+            flip();
+            currentpoint = pointB.transform;
+        }
+    }
+
+    private void flip()
+    {
+        Vector3 localscale = transform.localScale;
+        localscale.x *= -1;
+        transform.localScale = localscale;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+}
